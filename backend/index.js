3ce179c5228de2589b11fcd9d551749a0b75a4fa
Daksh Thakran <daksh.thakran05@gmail.com>
2026-03-27 10:32:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,7 @@ app.use(
       'https://wrap-up-one.vercel.app',
       'http://localhost:5173',
       'https://wrap-up-evolved.vercel.app',
+      'https://wrap-up-onechain.vercel.app',
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
